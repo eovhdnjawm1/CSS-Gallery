3a256101a03d940f7b44eedcb4a1ffa719d8e48c
YoungJinGo <skyyj32@naver.com>
2022-05-19 23:42:11 +0900

    Feat Basic Setting

--- css_gallery/src/App.js
+++ css_gallery/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Main from './components/main.js'
 
 function App() {
+
   return (
     <div className="App">
-
+      <Main />
     </div>
   );
 }
---
+++ css_gallery/src/components/main.js
@@ -0,0 +1,9 @@
+function MainContainer() {
+	return (
+		<div>
+			<h1>CSS Gallery</h1>
+		</div>
+	)
+}
+
+export default MainContainer;
