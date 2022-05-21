2a8b77666e5c55a62d85d1c9b74dd68c0dac756d
YoungJinGo <skyyj32@naver.com>
2022-05-21 21:39:28 +0900

    Feat github json 데이터 불러오기 성공

---
+++ css_gallery/src/components/Component3D.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+
+function Component3D() {
+
+	let cssImage = useState();
+
+	return (
+		<div >
+			<h1>3D 입니다</h1>
+			<div className='cssContainer'>
+							
+			</div>
+		</div>
+	)
+}
+
+export default Component3D;
--- css_gallery/src/components/main.js
+++ css_gallery/src/components/main.js
@@ -1,20 +1,39 @@
 import '../css/Main.css'
 import axios from 'axios';
+import Component3D from './Component3D.js'
 
 function MainContainer() {
+
+	const url = 'https://eovhdnjawm1.github.io/db'
+	const json3D = '/3D_data.json'
+	const jsonButton = '/button_data.json'
+	const jsonEffect = '/effect_data.json'
+	const jsonNav = '/navbar_data.json'
+
 	return (
 		<div className='mainContainer'>
 			<h1 className='mainTitle'>CSS Gallery</h1>
-			<Test />
-			
+			<button onClick={() => {
+				axios.get(url + json3D).then((result) => { 
+					console.log(result.data[0].title)
+				})
+				.catch(() => {
+					console.log('실패')
+				})
+			}}>버튼
+				
+			</button>
+			<Component3D />
 		</div>
 	)
 }
 
 
-async function Test() {
-	const test = await axios.get(`https://raw.githubusercontent.com/eovhdnjawm1/CSS-Gallery/main/css_gallery/my-json-server/db.json`);
-	console.log(test);
-}
+
+// async function Test() {
+// 	const test = await axios.get(`my-json-server.typicode.com/eovhdnjawm1/posts?id=${test}`);
+// 	const testJson = await test.json();
+// 	const JSON = testJson.data.
+// }
 
 export default MainContainer;
