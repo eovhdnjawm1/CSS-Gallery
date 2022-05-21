e75f09c77e621878278afa17e670dcc7b4c9e84d
YoungJinGo <skyyj32@naver.com>
2022-05-21 11:18:13 +0900

    Feat axios Update

--- css_gallery/src/components/main.js
+++ css_gallery/src/components/main.js
@@ -1,11 +1,20 @@
 import '../css/Main.css'
+import axios from 'axios';
 
 function MainContainer() {
 	return (
 		<div className='mainContainer'>
 			<h1 className='mainTitle'>CSS Gallery</h1>
+			<Test />
+			
 		</div>
 	)
+}
+
+
+async function Test() {
+	const test = await axios.get(`https://raw.githubusercontent.com/eovhdnjawm1/CSS-Gallery/main/css_gallery/my-json-server/db.json`);
+	console.log(test);
 }
 
 export default MainContainer;
