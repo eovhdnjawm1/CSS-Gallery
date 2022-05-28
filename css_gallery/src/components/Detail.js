60532e200a8ddaf6396bf8388f8e4f0229e0d663
YoungJinGo <skyyj32@naver.com>
2022-05-29 04:04:48 +0900

    Feat Params 제거

--- css_gallery/src/components/Detail.js
+++ css_gallery/src/components/Detail.js
@@ -1,15 +1,9 @@
 import '../css/Detail.css';
-import { useParams } from 'react-router-dom'
 
 function CssButton() {
-	// let { id } = useParams();
-	// let 찾은상품 = props.grapics.find(function (x) {
-	// 	return x.id == id;
-	// });
 
 	return (
 		<div>
-			{/* <h1> {찾은상품.title} </h1> */}
 			<div className='cards3D'>
 				<figure className='card'>
 					<figcaption className="card-title">Flower</figcaption>
