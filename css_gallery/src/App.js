9223e0dae3ba3505683967a8b9111163ed65497f
YoungJinGo <skyyj32@naver.com>
2022-05-31 10:53:23 +0900

    Feat Test 코드 제거

--- css_gallery/src/App.js
+++ css_gallery/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Main from './components/main.js'
 import {BrowserRouter} from 'react-router-dom'
-import Bounce from './components/BounceLoading.js'
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Main /> */}
-        <Bounce />
+        <Main />
       </BrowserRouter>
     </div>
   );
