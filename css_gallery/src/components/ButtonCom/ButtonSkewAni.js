7a70a67dc0c63da58865189e0750945072a0c5a5
YoungJinGo <skyyj32@naver.com>
2022-05-31 11:30:35 +0900

    Feat Button Skew 완성

---
+++ css_gallery/src/components/ButtonCom/ButtonSkewAni.js
@@ -0,0 +1,13 @@
+import '../../css/ButtonCom/ButtonSkewAni.css'
+
+function ButtonSkew() {
+	return (
+		<div>
+			<a href="#" className='ButtonSkewbutton'>
+				CLICK HERE!
+			</a>
+		</div>
+	)
+}
+
+export default ButtonSkew;
